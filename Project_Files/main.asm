e227cedfb01cca80d819cb0654d4f8c66a509894
Abdelrahman-Moubarak <57411478+Abdelrahman-Moubarak@users.noreply.github.com>
2020-04-01 13:06:29 +0200

    Read From file (Enhanced)

--- Project_Files/main.asm
+++ Project_Files/main.asm
@@ -2,29 +2,30 @@ INCLUDE Irvine32.inc
 .DATA
 BoardSize EQU 81
 filename BYTE "file.txt",0
-FileBufferSize Dword 97
+FileBufferSize EQU 97
 ArrayReadFile byte 97 dup(?)
 Board byte 81 dup(?)
 counter1 Dword 0
 .code
 main PROC
-	
+	mov edx, OFFSET filename
+	mov ebx, OFFSET ArrayReadFile
+	mov esi, OFFSET Board
 	call ReadMyFile
 	mov esi , OFFSET Board
-	mov edx , offset ArrayReadFile
+	mov edx , OFFSET ArrayReadFile
 	mov al , [esi+80]
 	call WriteChar
 	exit
 main ENDP
 
-ReadMyFile PROC  
-	mov edx, OFFSET filename
+ReadMyFile PROC  USES eax ebx ecx edx esi
+	
 	call OpenInputFile 
 	mov ecx, FileBufferSize
-	mov edx, OFFSET ArrayReadFile
+	mov edx, ebx
 	call ReadFromFile
-	mov esi, OFFSET Board
-	mov edx, OFFSET ArrayReadFile
+	mov edx, ebx
 	mov ecx , FileBufferSize
 	CopyArray:
 		inc counter1
