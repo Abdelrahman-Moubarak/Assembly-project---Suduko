a3ece72c2ed8b56941ad546af8ad6804bb0d98ae
alaahassan2022 <alaa.hassan.mahmoud.01@gmail.com>
2020-04-03 00:01:55 +0200

    Check & input

--- Project_Files/main.asm
+++ Project_Files/main.asm
@@ -2,19 +2,29 @@ INCLUDE Irvine32.inc
 .DATA
 BoardSize EQU 81
 filename BYTE "file.txt",0
+filenameofunsolved BYTE "fileUnsolved.txt",0
 FileBufferSize EQU 97
 ArrayReadFile byte 97 dup(?)
 Board byte 81 dup(?)
+BoardUser byte 81 dup(?)
 counter1 Dword 0
+CellRow dword ?
+CellCol dword ?
+CellIndexFinal dword ?
+wrongrowcol byte "Out Of Range Index!",0
+Answer byte ?
+Answerdword dword ?
 .code
 main PROC
 	mov edx, OFFSET filename
 	mov ebx, OFFSET ArrayReadFile
 	mov esi, OFFSET Board
 	call ReadMyFile
+	call InputUnsolvedBoard
 	mov esi , OFFSET Board
 	mov edx , OFFSET ArrayReadFile
 	mov al , [esi+71] ; helloo
+
 	call WriteChar
 	exit
 main ENDP
@@ -48,5 +58,92 @@ ReadMyFile PROC  USES eax ebx ecx edx esi
 	loop CopyArray
 	ret
 ReadMyFile ENDP
+
+InputUnsolvedBoard proc
+	mov edx, OFFSET filenameofUnsolved
+	mov ebx, OFFSET ArrayReadFile
+	mov esi, OFFSET BoardUser
+
+
+InputUnsolvedBoard ENDP
+
+;------------------CheckIfEqual proc
+InputAndCheck Proc uses esi ebx edx
+	
+	call InputRowCol
+	call CalcIndex
+	cmp CellIndexFinal, 80
+	
+	ja OutOfRange
+	
+	jmp InRange
+
+	OutOfRange:
+	mov edx, offset wrongrowcol
+	call writestring
+	call InputAndCheck
+
+	InRange:
+	mov esi, CellIndexFinal
+	cmp boardUser[esi], 0
+	
+	je ifUnknown
+	
+	jmp endoffn
+	ifUnknown:
+	call InputAns
+	mov esi, CellIndexFinal
+	mov bl, Answer
+	cmp board[esi], bl
+	jz ifEqual
+	;if not equal
+	
+	jmp endoffn
+
+	ifEqual: ; if equal
+	mov esi, CellIndexFinal
+	mov bl, Answer
+	mov BoardUser[esi], bl
+	endoffn:
+	;call displayBoard
+	ret
+InputAndCheck ENDP
+;------------------End of CheckIfEqual proc
+
+;-------------------CalcIndex Proc
+CalcIndex Proc uses eax
+	mov ebx, CellRow
+	mov ecx, 8
+	RowLoop:
+	add ebx, CellRow
+	Loop RowLoop
+
+	mov CellRow, ebx
+	add eax, CellCol
+	mov CellIndexFinal, eax
+	ret
+CalcIndex ENDP
+;-------------------End of CalcIndex Proc
+
+;-------------------InputRowCol Proc
+InputRowCol PROC uses eax
+	call Readdec
+	mov CellRow, eax
+	call Readdec
+	mov CellCol, eax
+	ret
+InputRowCol ENDP
+;-------------------End of InputRowCol Proc
+
+;-------------------InputAns Proc
+InputAns Proc uses eax esi ebx
+	call Readdec
+	mov answerdword, eax
+	mov esi, offset answerdword
+	mov bl, byte ptr[esi]
+	mov answer,bl
+	ret
+InputAns ENDP
+;-------------------End of InputAns Proc
 
 END main
